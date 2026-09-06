c9a51e6bff647ff29b78441f6a229867f11599ef
yoCruzer <8610143+yoCruzer@users.noreply.github.com>
2026-09-07 07:24:33 +0800

    Initial Commit

---
+++ FiveMinuteExplorer/FiveMinuteExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveMinuteExplorerApp.swift
+//  FiveMinuteExplorer
+//
+//  Created by hanghang on 2026/9/7.
+//
+
+import SwiftUI
+
+@main
+struct FiveMinuteExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
